580718c06ba16c33111d4d7c6aaaaae7ee1ac6e0
Control3022 <102419944+Control3022@users.noreply.github.com>
2022-03-26 14:19:39 +0000

    Update dds.py

--- dds.py
+++ dds.py
@@ -1,15 +1,16 @@
 user_agent_list = [
-'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
+'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36',
 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
 ]
+headesr = {'User-Agent':str(random.choice(user_agent_list))}
 import requests, random
 global url
 while True:
     try:
-        url = requests.get('https://pastebin.com/raw/SUWiWguE').text
+        url = requests.get('https://pastebin.com/raw/SUWiWguE', headers=headesr).text
         print(url)
         break
     except:
@@ -36,7 +37,7 @@ for _ in range(20):
     threads.append(td)
 while True:
     try:
-        a=requests.get('https://pastebin.com/raw/SUWiWguE').text
+        a=requests.get('https://pastebin.com/raw/SUWiWguE', headers=headesr).text
         if a not in url:
             url=a
             print(a+' '+url)
