b3662da1772cae32db5c377addb25df3f8f2a114
Control3022 <102419944+Control3022@users.noreply.github.com>
2022-03-26 14:28:39 +0000

    Update dds.py

--- dds.py
+++ dds.py
@@ -1,12 +1,9 @@
 import random
 user_agent_list = [
 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36',
-'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
-'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
-'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
-'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
 ]
 headesr = {'User-Agent':str(random.choice(user_agent_list))}
+print(headesr)
 import requests, random
 global url
 while True:
